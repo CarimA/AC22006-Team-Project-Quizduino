ee9d73d6c7b0c45a9ff590d1a255293f1e3b2746
suuf <skyllakia@gmail.com>
2015-03-17 11:19:50 +0000

    servo

---
+++ Code/arduino program/servo/servo.ino
@@ -0,0 +1,23 @@
+#include <Servo.h> 
+
+const int servoPin = 8;
+
+Servo myservo;  // create servo object to control a servo
+
+void setup()
+{
+  myservo.attach(servoPin);  // attaches the servo on pin 8 to the servo object  
+  Serial.begin(9600);
+}
+
+void loop()
+{
+
+    // go from 0-180 degrees
+  for(int i=0; i < 180; i++)   
+  { 
+    myservo.write(i);                // tell servo to move to new position 
+    delay(111);                      // wait (20 seconds/180?)
+  }
+  
+}
